278e8f2cb38995f19aa4f4a2ea8716437e96d520
Diego Carrion <dc.rec1@gmail.com>
2015-05-31 20:31:11 -0300

    specify deploy email

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ ember build --environment production
 cd dist
 git init
 git config user.name "Travis CI"
-git config user.email "<you>@<your-email>"
+git config user.email "deploy@travisci.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/dcrec1/github-user-events.git" master:gh-pages > /dev/null 2>&1
