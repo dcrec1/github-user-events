90fb83f385bfef7834d926381fb6ea8af4c2fac6
Diego Carrion <dc.rec1@gmail.com>
2015-05-31 17:03:49 -0300

    configured deployment

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+ember build --environment production
+cd dist
+git init
+git config user.name "Travis CI"
+git config user.email "<you>@<your-email>"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/dcrec1/github-user-events.git" master:gh-pages > /dev/null 2>&1
