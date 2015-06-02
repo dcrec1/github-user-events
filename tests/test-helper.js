428a179cef142d132d7c14f545538498243efb72
Diego Carrion <dc.rec1@gmail.com>
2015-06-01 23:52:25 -0300

    add ember-betamax

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,9 @@
 import resolver from './helpers/resolver';
+import insertCassette from './helpers/insert-cassette';
+import cassette from './helpers/cassette';
 import {
   setResolver
 } from 'ember-qunit';
 
 setResolver(resolver);
+insertCassette(cassette);
